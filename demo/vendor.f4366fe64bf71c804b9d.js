22c1987d63278f25f3ab066395b7f187e12f0ec9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-30 19:08:11 +0800

    Deploy zw277856645/ngx-virtual-scroll to github.com/zw277856645/ngx-virtual-scroll.git:gh-pages

---
+++ demo/vendor.f4366fe64bf71c804b9d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{D89A:function(n,e,o){},Q7f2:function(n,e,o){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),o("D89A"),o("h+6X")},"h+6X":function(n,e,o){}},[["Q7f2",0]]]);
+//# sourceMappingURL=vendor.f4366fe64bf71c804b9d.js.map
---
+++ js/menu-wc.js
@@ -0,0 +1,101 @@
+'use strict';
+
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">ngx-virtual-scroll documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>入门指南</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>概述
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>手册
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="license.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>LICENSE
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                    <li class="chapter modules">
+                        <a data-type="chapter-link" href="modules.html">
+                            <div class="menu-toggler linked" data-toggle="collapse" ${ isNormalMode ?
+                                'data-target="#modules-links"' : 'data-target="#xs-modules-links"' }>
+                                <span class="icon ion-ios-archive"></span>
+                                <span class="link-name">模块列表</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                        </a>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="modules-links"' : 'id="xs-modules-links"' }>
+                            <li class="link">
+                                <a href="modules/VirtualScrollModule.html" data-type="entity-link">VirtualScrollModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#components-links-module-VirtualScrollModule-e2cb99e44b98922fcdc38f2d3573e809"' : 'data-target="#xs-components-links-module-VirtualScrollModule-e2cb99e44b98922fcdc38f2d3573e809"' }>
+                                            <span class="icon ion-md-cog"></span>
+                                            <span>组件列表</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-VirtualScrollModule-e2cb99e44b98922fcdc38f2d3573e809"' :
+                                            'id="xs-components-links-module-VirtualScrollModule-e2cb99e44b98922fcdc38f2d3573e809"' }>
+                                            <li class="link">
+                                                <a href="components/VirtualScrollComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">VirtualScrollComponent</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                            </li>
+                </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#classes-links"' :
+                            'data-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>类列表</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/ItemChanges.html" data-type="entity-link">ItemChanges</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/RefreshConfig.html" data-type="entity-link">RefreshConfig</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/RefreshItemsConfig.html" data-type="entity-link">RefreshItemsConfig</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/ScrollConfig.html" data-type="entity-link">ScrollConfig</a>
+                            </li>
+                        </ul>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
